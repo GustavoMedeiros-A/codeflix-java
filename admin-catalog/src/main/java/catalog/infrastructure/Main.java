a89d43afe6f0180b16bdf8b22792fafdc6d35571
GustavoMedeiros-A <gustavomedeiros15@outlook.com.br>
2024-06-19 11:43:43 -0300

    feat: try to config profiles

--- admin-catalog/src/main/java/catalog/infrastructure/Main.java
+++ admin-catalog/src/main/java/catalog/infrastructure/Main.java
@@ -3,6 +3,7 @@ package catalog.infrastructure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.env.AbstractEnvironment;
 
 import catalog.infrastructure.configuration.WebServerConfig;
 
@@ -10,6 +11,7 @@ import catalog.infrastructure.configuration.WebServerConfig;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Print");
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
--- start.sh
+++ start.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+
+
+is_container_running() {
+    docker ps --format '{{.Names}}' | grep -q "^$1$"
+}
+
+CONTAINER_NAME="adm_videos_mysql"
+
+if is_container_running $CONTAINER_NAME; then
+    echo "Container $CONTAINER_NAME already running"
+else
+    echo "Container $CONTAINER_NAME is not running. Starting docker-compose up..."
+    docker-compose up -d 
+fi
+
+sleep 1
+
+docker ps 
+
+sleep 1
+
 ./gradlew :admin-catalog:bootRun
 
-# or this  java -jar admin-catalog/build/libs/application.jar 
+# java -jar admin-catalog/build/libs/application.jar 
